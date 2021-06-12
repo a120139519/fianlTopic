219084582797f48febf58989381e37362d4e7217
TimLiu1987 <59197061+TimLiu1987@users.noreply.github.com>
2021-06-12 18:21:02 +0800

    Update MineSweeper.h

--- MineSweeper.h
+++ MineSweeper.h
@@ -21,7 +21,7 @@ public:
 	void setMode2(int, int);
 	void selectMode1(int, int);
 	void selectMode2(int, int);
-	void poi(int, int);//不知道要取甚麼名字...反正是當選擇的位置為零時，翻開附近的牌
+	void RecursionOpen(int, int);//當選擇的位置為零時，翻開附近的牌
 	bool isWin();//檢查是否勝利
 	void selectBomb();
 	void setSelect(int, int);//將該位置設定已被選過
@@ -29,11 +29,10 @@ public:
 	bool isSelect(int, int);//檢查該位置是否被選過 是則回傳1
 	int getMineSweeperOfNum(int, int);//回傳該位置炸彈數量
 	MineSweeper(int, int, int);
-	void createMineSweeper(int,int);
-	void createBomb(int,int);
+	void createMineSweeper(int, int);
+	void createBomb(int, int);
 	bool isMode2(int, int);
 	void printMineSweeper();
 	int checkBomb(int, int);//檢查周圍炸彈數量
 	~MineSweeper();
 };
-
